c445f2ffd4991beef9bc48016be1fb9936360705
Eros Eloi <eroseloi92@gmail.com>
2024-04-20 19:00:55 -0300

    Adicao do teste divisao por zero

--- src/main/java/br/com/calculadora/Calculadora.java
+++ src/main/java/br/com/calculadora/Calculadora.java
@@ -6,20 +6,20 @@ public class Calculadora {
 		double resultado = a + b;
 		return resultado;
 	}
-	
+
 	public double subtracao(double a, double b) {
 		double resultado = a - b;
 		return Math.abs(resultado);
 	}
-	
+
 	public double divisao(double a, double b) {
 		double resultado = a / b;
 		return resultado;
 	}
-	
+
 	public double multiplicacao(double a, double b) {
 		double resultado = a * b;
 		return resultado;
 	}
-	
+
 }
--- src/test/java/br/com/calculadora/CalculadoraTeste.java
+++ src/test/java/br/com/calculadora/CalculadoraTeste.java
@@ -6,63 +6,72 @@ import org.junit.jupiter.api.Test;
 
 class CalculadoraTeste {
 
-	
-	
-	//Teste para Soma
+	// Teste para Soma
 	@Test
 	public void SomaTest() {
 		Calculadora calculadora = new Calculadora();
 		int a = 5;
 		int b = 7;
-		
-		double resultadoEsperado = 12; //resultado esperado do metodo
+
+		double resultadoEsperado = 12; // resultado esperado do metodo
 		double resultadoReal = calculadora.soma(a, b);
-		
-		//Comparacao dos resultados
-		assertEquals(resultadoEsperado, resultadoReal, 0.0001); 
-		
+
+		// Comparacao dos resultados
+		assertEquals(resultadoEsperado, resultadoReal, 0.0001);
+
 	}
-	
-	//Teste para Subtracao
+
+	// Teste para Subtracao
 	@Test
 	public void SubtracaoTest() {
 		Calculadora calculadora = new Calculadora();
 		double a = 5;
 		double b = 7;
-		
-		double resultadoEsperado = 2;//resultado esperado do metodo
+
+		double resultadoEsperado = 2;// resultado esperado do metodo
 		double resultadoReal = calculadora.subtracao(a, b);
-		
-		//Comparacao dos resultados
+
+		// Comparacao dos resultados
 		assertEquals(resultadoEsperado, resultadoReal, 0.001);
 	}
-	
-	//Teste para multipicacao
+
+	// Teste para multipicacao
 	@Test
 	public void multiplicacaoTest() {
 		Calculadora calculadora = new Calculadora();
 		double a = 5;
 		double b = 7;
-		
-		double resultadoEsperado = 35;//resultado esperado do metodo
+
+		double resultadoEsperado = 35;// resultado esperado do metodo
 		double resultadoReal = calculadora.multiplicacao(a, b);
-		
-		//Comparacao dos resultados
+
+		// Comparacao dos resultados
 		assertEquals(resultadoEsperado, resultadoReal, 0.001);
 	}
-	
-	//Teste para Divisao
+
+	// Teste para Divisao
 	@Test
 	public void divisaoTeste() {
 		Calculadora calculadora = new Calculadora();
 		double a = 10;
 		double b = 2;
-		
-		double resultadoEsperado = 5;//resultado esperado do metodo
+
+		double resultadoEsperado = 5;// resultado esperado do metodo
 		double resultadoReal = calculadora.divisao(a, b);
-		
-		//Comparacao dos resultados
+
+		// Comparacao dos resultados
 		assertEquals(resultadoEsperado, resultadoReal, 0.001);
+	}
+	
+	@Test
+	public void DivisaoPorZero() {
+		Calculadora calculadora = new Calculadora();
+		 double a = 10;
+		 double b = 0;
+		 
+		 calculadora.divisao(a, b);
+		 //O teste espera que uma execao ArithmeticException seja lancada
+		 
 	}
 
 }
